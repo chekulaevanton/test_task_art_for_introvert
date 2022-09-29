108e6c7ed72cd502049f7f110f3be59ec59323b5
Anton Chekulaev <chekulaevanton1@gmail.com>
2022-09-29 14:11:00 +0400

    feat(errors): add AlreadyExistsError

--- utils/errors.go
+++ utils/errors.go
@@ -1,22 +1,25 @@
 package utils
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 /* Value Error
  * Описывает состояние, когда поля структуры заданы не верно.
  */
 type ValueError struct {
-	Err error
+    Err error
 }
 
 func NewValueError(err string) ValueError {
-	return ValueError{
-		Err: errors.New(err),
-	}
+    return ValueError{
+        Err: errors.New(err),
+    }
 }
 
 func (e ValueError) Error() string {
-	return e.Err.Error()
+    return e.Err.Error()
 }
 
 /* Not Found Error
@@ -24,15 +27,33 @@ func (e ValueError) Error() string {
  * эквивалентра HTTP коду 404.
  */
 type NotFoundError struct {
-	Err error
+    Err error
 }
 
-func NewNotFoundError(err string) NotFoundError {
-	return NotFoundError{
-		Err: errors.New(err),
-	}
+func NewNotFoundError(id int) NotFoundError {
+    return NotFoundError{
+        Err: fmt.Errorf("Course with id=%v not found", id),
+    }
 }
 
 func (e NotFoundError) Error() string {
-	return e.Err.Error()
+    return e.Err.Error()
+}
+
+/* Already Exists Error
+ * Описывает состояние, когда запрошенный ресурс требуется создать,
+ * но уже существует.
+ */
+ type AlreadyExistsError struct {
+    Err error
+}
+
+func NewAlreadyExistsError(id int) AlreadyExistsError {
+    return AlreadyExistsError{
+        Err: fmt.Errorf("Course with id=%v already exits", id),
+    }
+}
+
+func (e AlreadyExistsError) Error() string {
+    return e.Err.Error()
 }
